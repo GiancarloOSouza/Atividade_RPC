0374467fb983212173beded9653a5833be921f4f
Giancarlo.Souza <giancarlo.souza@unifesspa.edu.br>
2022-09-27 05:34:42 -0300

    Feat: Servidor da Azul criado

--- Server/Server.py
+++ Server/Server.py
@@ -1,12 +1,18 @@
 from xmlrpc.server import SimpleXMLRPCServer
 from xmlrpc.server import SimpleXMLRPCRequestHandler
+import xmlrpc.client
+
+
+
+
+
 
 # Restrict to a particular path.
 class RequestHandler(SimpleXMLRPCRequestHandler):
     rpc_paths = ('/RPC2',)
 
 # Create server
-with SimpleXMLRPCServer(('localhost', 8000),
+with SimpleXMLRPCServer(('192.168.0.191', 8000),
                         requestHandler=RequestHandler) as server:
     server.register_introspection_functions()
 
@@ -18,6 +24,13 @@ with SimpleXMLRPCServer(('localhost', 8000),
     def adder_function(x, y):
         return x + y
     server.register_function(adder_function, 'add')
+
+
+    def serverAzul(x, y):
+        serverAzul = xmlrpc.client.ServerProxy('http://192.168.0.104:11290')
+        return serverAzul.zero(x,y)
+
+    server.register_function(serverAzul, 'serverAzul')
 
     # Register an instance; all the methods of the instance are
     # published as XML-RPC methods (in this case, just 'mul').
--- Server/ServerAzul.py
+++ Server/ServerAzul.py
@@ -1,5 +1,21 @@
 from xmlrpc.server import SimpleXMLRPCServer
 from xmlrpc.server import SimpleXMLRPCRequestHandler
+from Hospedagens import Hospe
+
+hospe = []
+
+hospe1 = Hospe("Marceio", "Ibis", 500)
+hospe2 = Hospe("Sao Paulo", "Cabana o seu zé", 600)
+hospe3 = Hospe("Fernando de noronha", "", 700)
+hospe4 = Hospe("Belo Monte", "M", 800)
+
+hospe.append(hospe1) 
+hospe.append(hospe2)
+hospe.append(hospe3)
+hospe.append(hospe4)
+
+
+
 
 # Restrict to a particular path.
 class RequestHandler(SimpleXMLRPCRequestHandler):
@@ -10,11 +26,15 @@ with SimpleXMLRPCServer(('192.168.0.104', 11290),
                         requestHandler=RequestHandler) as server:
     server.register_introspection_functions()
 
- 
-    def tudoZero(x, y):
-        m = (x*y) * 0
-        return  m
+    def barato(valor):
+        for i in hospe:
+            if(i.valor < valor):
+                print
+                print("Na cidade " + i.cidade + "\n, O hotel "+ i.hotel+ "esta com o valor de: " +i.valor)
+
+
     
-    server.register_function(tudoZero, 'zero')
+    
+    server.register_function(barato, 'barato')
 
     server.serve_forever()
